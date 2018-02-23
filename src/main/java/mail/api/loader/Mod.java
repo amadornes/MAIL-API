160755b360aa7f6eb04f54e32bea1c18f6d3afae
Amadornes <amadornes@gmail.com>
2018-02-23 19:46:04 +0100

    Final touches on the initial revision of the mod loader API

--- src/main/java/mail/api/loader/Mod.java
+++ src/main/java/mail/api/loader/Mod.java
@@ -1,9 +1,56 @@
 package mail.api.loader;
 
+import mail.movetolib.version.Version;
+
+import java.util.Set;
+
+/**
+ * Object that represents a mod.
+ *
+ * @see Loader
+ * @see ModProvider
+ */
 public interface Mod {
 
+    /**
+     * Gets the unique identifier of this mod.
+     */
+    String getModID();
+
+    /**
+     * Gets the display name of this mod.
+     */
+    String getName();
+
+    /**
+     * Gets the version of this mod according to the SEMVER standard.
+     */
+    Version getVersion();
+
+    /**
+     * Prototype for a mod used during the identification phase for compatibility purposes.
+     */
     interface Prototype {
 
+        /**
+         * Gets the unique identifier of this mod.
+         */
+        String getModID();
+
+        /**
+         * Gets the display name of this mod.
+         */
+        String getName();
+
+        /**
+         * Gets the version of this mod according to the SEMVER standard.
+         */
+        Version getVersion();
+
+        /**
+         * Gets the set of dependencies for this mod.
+         */
+        Set<String> getDependencies();
     }
 
 }
--- src/main/java/mail/api/loader/ModContainer.java
+++ src/main/java/mail/api/loader/ModContainer.java
@@ -1,19 +1,39 @@
 package mail.api.loader;
 
+import mail.api.serial.DataStructure;
+
 import java.nio.file.FileSystem;
 import java.nio.file.Path;
 
+/**
+ * Object that represents a container (jar or directory) with a mod in it.
+ *
+ * @see Loader
+ * @see ModProvider
+ */
 public interface ModContainer {
 
+    /**
+     * Gets the {@link Path} to the container.
+     */
     Path getPath();
 
-    FileSystem getFiles();
+    /**
+     * Gets a representation of the container's {@link FileSystem}.<br/>
+     * To be used in combination with {@link #getFileSystemRoot()}.
+     */
+    FileSystem getFileSystem();
 
-    Metadata getMetadata();
+    /**
+     * Gets the root {@link Path} of this container's contents inside its {@link FileSystem}.<br/>
+     * To be used in combination with {@link #getFileSystem()}.
+     */
+    Path getFileSystemRoot();
 
-    interface Metadata {
-
-    }
+    /**
+     * Gets a parsed representation of this mod's metadata file.
+     */
+    DataStructure getMetadata();
 
 }
 
